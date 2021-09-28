8cb8cf248657596c1fcad58506a0c0e6dae1fad9
heoboseong7 <boseong844@gmail.com>
2021-09-28 15:17:11 +0900

    Add: input validation

--- assignment03.sh
+++ assignment03.sh
@@ -1,8 +1,20 @@
+numberRE='^[0-9]+$'
+
+if ! [[ $1 =~ $numberRE ]] ; then
+	echo "$1 is invalid input"
+	exit 1
+fi
+if ! [[ $2 =~ $numberRE ]] ; then
+	echo "$2 is invalid input"
+	exit 1
+fi
+
+
 for i in $(seq 1 $1)
 do
-for j in $(seq 1 $2)
-do
-echo -n "$i*$j=`expr $i \* $j` " 
-done
-echo
+	for j in $(seq 1 $2)
+	do
+		echo -n "$i*$j=`expr $i \* $j` " 
+	done
+	echo
 done
